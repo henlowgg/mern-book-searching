317cde5ffe9d67f22c80d54f5cc133640690d4ec
Jack Ault <henlowgg@outlook.com>
2021-11-20 13:02:00 -0500

    auth set up, working on server.js to connect it all together

--- server/server.js
+++ server/server.js
@@ -1,21 +1,12 @@
 const express = require('express');
 const path = require('path');
+// will have to import Apollo Server from apollo-server-express here
+const { ApolloServer } = require('apollo-server-express');
+
+// after this line, we can use gql from apollo-server-express
+// next will have to import typeDefs and resolvers from schema
+
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
+const {authMiddleware} = require('./utils/auth');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
