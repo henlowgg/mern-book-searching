ebc0c12c78ecd210b413e01a54295c31a6ce8f9a
Jack Ault <henlowgg@outlook.com>
2021-11-20 12:46:41 -0500

    resolvers template for what needs to be written in

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,11 @@
 const {User} = require('../models')
 const {AuthenticationError} = require('apollo-server-express');
 const {signToken} = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+    },
+    Mutation: {
+    },
+
+    module.exports = resolvers;
